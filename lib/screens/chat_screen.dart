535e098a79c9388e4c0ae478d1e991af01d3a927
cv692001 <19uec128@lnmiit.ac.in>
2020-10-21 14:02:49 +0530

    FInal commit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -11,8 +11,9 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final _firestore = Firestore.instance;
+  final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
+  final messagingTextController = TextEditingController();
 
   String messageText;
   User loggedInUser;
@@ -33,6 +34,22 @@ class _ChatScreenState extends State<ChatScreen> {
       }
     } catch (e) {
       print(e);
+    }
+  }
+
+  // void getMessages() async {
+  //   final messages = await _firestore.collection('messages').get();
+
+  //   for (var message in messages.docs) {
+  //     print(message.data());
+  //   }
+  // }
+
+  void messageStream() async {
+    await for (var snapshots in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshots.docs) {
+        print(message.data());
+      }
     }
   }
 
@@ -57,6 +74,42 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+              stream: _firestore
+                  .collection('messages')
+                  .orderBy('time', descending: false)
+                  .snapshots(),
+              builder: (context, snapshots) {
+                if (!snapshots.hasData) {
+                  print('jo');
+                }
+                final messages = snapshots.data.docs.reversed;
+                List<MessageBubble> messageWidgets = [];
+                for (var message in messages) {
+                  final messageText = message.data()['text'];
+                  final messageSender = message.data()['sender'];
+                  final messageTime = message.data()['time'] as Timestamp;
+                  final currentuser = loggedInUser.email;
+
+                  final messageWidget = MessageBubble(
+                    text: messageText,
+                    sender: messageSender,
+                    isME: currentuser == messageSender,
+                    time: messageTime,
+                  );
+
+                  messageWidgets.add(messageWidget);
+                }
+
+                return Expanded(
+                  child: ListView(
+                    reverse: true,
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                    children: messageWidgets,
+                  ),
+                );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -64,6 +117,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messagingTextController,
                       onChanged: (value) {
                         messageText = value;
                       },
@@ -72,10 +126,13 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () {
+                      messagingTextController.clear();
+
                       _firestore.collection("messages").add(
                         {
                           'text': messageText,
                           'sender': loggedInUser.email,
+                          'time': FieldValue.serverTimestamp()
                         },
                       );
                     },
@@ -90,6 +147,63 @@ class _ChatScreenState extends State<ChatScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble({this.sender, this.text, this.isME, this.time});
+
+  final String sender;
+  final String text;
+  final bool isME;
+  final Timestamp time;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment:
+          isME ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                " $sender",
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black54,
+                ),
+              ),
+              Material(
+                borderRadius: isME
+                    ? BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                        topLeft: Radius.circular(30),
+                      )
+                    : BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                elevation: 5,
+                color: isME ? Colors.lightBlueAccent : Colors.white,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Text(
+                    text,
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
